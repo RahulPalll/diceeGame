2dce2de244b0419a6ee105f6ce9c572a84330803
RahulPal <pal44rahul@gmail.com>
2023-04-04 18:13:09 +0530

    Font size change

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 
 if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player1 Wins!";
 }
 else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "🚩Player2 Wins!";
 }
 else{
     document.querySelector("h1").innerHTML="Draw!";
